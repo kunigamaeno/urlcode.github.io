2b8cdca53a6411d347288e5d9ba33c8a655beb8a
bluefuture <madara.tenmoku.549.549@gmail.com>
2017-03-27 18:34:54 +0900

    codemirror load. B.emit('cmload',el);return cm;

---
+++ cmload.js
@@ -0,0 +1,26 @@
+T([
+ "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.24.2/codemirror.min.css"
+,"https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.24.2/theme/twilight.min.css"
+,`.CodeMirror{ height:100%;}
+.cm-s-twilight .CodeMirror-gutters {background: #222;border-right: 1px solid #333;}
+.CodeMirror.cm-s-twilight {color: #aaa;}`
+,"https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.24.2/codemirror.min.js" 
+]).then((d)=>{
+/*cmload.js*/
+B.on('cmload',function(el){
+    var myCodeMirror = CodeMirror(el, {
+      value: '',
+      mode:  "markdown",
+     lineNumbers :true,
+     lineWrapping: true,
+     theme:"twilight"
+    });
+  /*
+    var fdata=()=>{return myCodeMirror.getValue()} 
+     myCodeMirror.on('change',function(){ myCodeMirror.getValue() })
+     myCodeMirror.on('gutterClick',function(){console.log(arguments)}); 
+  */
+  return myCodeMirror;
+ });
+ 
+ });
